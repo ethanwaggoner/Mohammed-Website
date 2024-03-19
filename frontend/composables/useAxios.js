95d22f1f87c18082502cd357090ee8269848cc76
ethanwaggoner <ethanjwaggoner@gmail.com>
2024-03-19 18:28:37 -0400

    User store allowing for login/logout

--- frontend/composables/useAxios.js
+++ frontend/composables/useAxios.js
@@ -1,23 +1,34 @@
-import { useCookie } from '#app'
+import { parseCookies } from 'cookie';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const axios = nuxtApp.$axios.create({
     baseURL: 'https://www.mohammedkhalid.com',
     withCredentials: true,
-  })
+  });
 
   axios.interceptors.request.use((config) => {
-    const csrfToken = useCookie('csrftoken').value
-    if (csrfToken) {
-      config.headers['X-CSRFToken'] = csrfToken
+    // Conditionally handle SSR and client-side
+    if (process.server) {
+      const req = nuxtApp.ssrContext.req;
+      const cookies = parseCookies(req.headers.cookie);
+      const csrfToken = cookies['csrftoken'];
+      if (csrfToken) {
+        config.headers['X-CSRFToken'] = csrfToken;
+      }
+    } else {
+      // Client-side handling with useCookie
+      const csrfToken = useCookie('csrftoken').value;
+      if (csrfToken) {
+        config.headers['X-CSRFToken'] = csrfToken;
+      }
     }
-    return config
-  })
+    return config;
+  });
 
   axios.interceptors.response.use(
     (response) => response,
     (error) => Promise.reject(error)
-  )
+  );
 
-  nuxtApp.provide('axios', axios)
-})
+  nuxtApp.provide('axios', axios);
+});
