1f649e3016b12ae47e0b867d64896aac262d56c3
ethanwaggoner <ethanjwaggoner@gmail.com>
2024-03-30 11:34:06 -0400

    Backend settings bug fix

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -1,10 +1,6 @@
 import os
 from pathlib import Path
-import environ
 
-
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'CHANGEMEEEEEEEEEEEEEEE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.tenantgossip.com']
+ALLOWED_HOSTS = ['www.localhost:3000/']
 
 
 # Application definition
