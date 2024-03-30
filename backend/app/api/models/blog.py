b425a2660dda9fcc1df5c38824feb695690a06f3
ethanwaggoner <ethanjwaggoner@gmail.com>
2024-03-30 11:04:31 -0400

    Backend API Blog model bugfix

--- backend/app/api/models/blog.py
+++ backend/app/api/models/blog.py
@@ -5,9 +5,9 @@ from django.conf import settings
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField(max_length=1000)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='posts')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.title
