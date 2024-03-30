367e6366a7447a6f753af175136dca59ddd6cba5
ethanwaggoner <ethanjwaggoner@gmail.com>
2024-03-30 10:59:13 -0400

    Backend API Blog and Comment models

---
+++ backend/app/api/models/blog.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField(max_length=1000)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
---
+++ backend/app/api/models/comment.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+
+
+class Comment(models.Model):
+    body = models.TextField()
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
